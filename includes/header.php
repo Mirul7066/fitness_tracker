68bc6c6fedc7e86fed7c16e80a632c9a5a3ca787
Mirul <amirulirfan.mka@gmail.com>
2025-06-23 13:25:41 +0800

    make some changes

--- includes/header.php
+++ includes/header.php
@@ -9,10 +9,8 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <title>Fitness Tracker</title>
 
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet"> 
 </head>
 <body>
@@ -25,27 +23,32 @@ if (session_status() === PHP_SESSION_NONE) {
         </button>
 
         <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+
             <ul class="navbar-nav mx-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="view_entries.php">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="goals.php">Goals</a>
-                </li>
+                <li class="nav-item"><a class="nav-link" href="view_entries.php">Dashboard</a></li>
+                <li class="nav-item"><a class="nav-link" href="goals.php">Goals</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
             </ul>
 
+            <?php if (isset($_SESSION['username'])): ?>
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <span class="nav-link text-white">Hello, <?= htmlspecialchars($_SESSION['username']) ?></span>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Logout</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-user-circle me-1"></i><?= htmlspecialchars($_SESSION['username']) ?>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                        <li><a class="dropdown-item" href="edit_profile.php"><i class="fas fa-user-edit me-2"></i>Edit Profile</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item text-danger" href="logout.php"><i class="fas fa-sign-out-alt me-2"></i>Logout</a></li>
+                    </ul>
                 </li>
             </ul>
+            <?php endif; ?>
         </div>
     </div>
 </nav>
 
 <div class="container mt-4">
+
 
 
